5b225b1873fbc1c00294fc2a1761e6f794f8c2d8
Gabriel Duarte <gabriel160499@gmail.com>
2017-10-26 14:08:50 -0200

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static string[] arguments = Environment.GetCommandLineArgs();
+        private static Socket clientSocket;
+
+        private static void Main(string[] args)
+        {
+            //IP TO CONNECT
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            //PORT TO CONNECT
+            int port = 8007;
+            Program.clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                Program.clientSocket.Connect((EndPoint)new IPEndPoint(address, port));
+            }
+            catch
+            {
+                return;
+            }
+            while (0 == 0)
+            {
+                string msg = Console.ReadLine();
+                if (msg != null)
+                    send(msg);
+            }
+            /*After connect to server, you send message to server using: send("DATA TO SEND");*/
+        }
+
+        static async void send(string SendThis)
+        {
+
+            clientSocket.Send(Encoding.ASCII.GetBytes(SendThis));
+            /*TO AUTO-SENT OF THE FIRST ARGUMENT
+             * clientSocket.Send(Encoding.ASCII.GetBytes(Program.arguments[1]));*/
+            await Task.Delay(100);
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+namespace Server
+{
+    class Program
+    {
+        private static byte[] result = new byte[1024];
+        static Socket serverSocket;
+        static void Main(string[] args)
+        {
+            //This server IP(Default = 127.0.0.1[LOCAL])
+            IPAddress server_ip = IPAddress.Parse("127.0.0.1");
+            //This server PORT(Recommend: Using ports that the system is not using)
+            int port = 8007;
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                serverSocket.Bind(new IPEndPoint(server_ip, port));
+                serverSocket.Listen(10);
+            }
+            catch 
+            {
+                return;
+            }
+
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                Thread receive_thread = new Thread(ReceiveMessage);
+                receive_thread.Start(clientSocket);
+            }
+        }
+        static void ListenClientConnect()
+        {
+         
+        }
+        static void ReceiveMessage(object clientSocket)
+        {
+            Socket client_socket = (Socket)clientSocket;
+            while (true)
+            {
+                try
+                {
+                    int receiveNumber = client_socket.Receive(result);
+                    //msg = The message recived by client;
+                    string msg = Encoding.ASCII.GetString(result, 0, receiveNumber);
+                 /*this.Dispatcher.Invoke(() => {
+                   Execute Functions in Form or WPF Page. Example: Button Click, Void, etc...       
+                  });*/
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
