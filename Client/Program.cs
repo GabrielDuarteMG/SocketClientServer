de3e1d875a5c00d49039396ad24e0d96397e2a8b
Gabriel Duarte <gabriel160499@gmail.com>
2017-10-31 13:16:35 -0200

    Update Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,7 +8,9 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        //Get arguments before send to server
         public static string[] arguments = Environment.GetCommandLineArgs();
+        //Declare Socket client
         private static Socket clientSocket;
 
         private static void Main(string[] args)
@@ -20,13 +22,16 @@ namespace ConsoleApp1
             Program.clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try
             {
+                //Connect server
                 Program.clientSocket.Connect((EndPoint)new IPEndPoint(address, port));
             }
             catch
             {
+                //If Don't connect to server, this program return and close.
                 return;
             }
-            while (0 == 0)
+            //Loop Send Message
+            while (true)
             {
                 string msg = Console.ReadLine();
                 if (msg != null)
@@ -37,10 +42,11 @@ namespace ConsoleApp1
 
         static async void send(string SendThis)
         {
-
+            //Client send the message if connection is stable
             clientSocket.Send(Encoding.ASCII.GetBytes(SendThis));
             /*TO AUTO-SENT OF THE FIRST ARGUMENT
              * clientSocket.Send(Encoding.ASCII.GetBytes(Program.arguments[1]));*/
+            //Delay to bytes return and the program don't crash
             await Task.Delay(100);
         }
     }
