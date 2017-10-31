334c75396623fb53751b811be015e25554b332dc
Gabriel Duarte <gabriel160499@gmail.com>
2017-10-31 14:28:54 -0200

    Update Program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -7,7 +7,9 @@ namespace Server
 {
     class Program
     {
+        // Declare bytes to recieve message
         private static byte[] result = new byte[1024];
+        //  Declare new Socket Server
         static Socket serverSocket;
         static void Main(string[] args)
         {
@@ -15,20 +17,25 @@ namespace Server
             IPAddress server_ip = IPAddress.Parse("127.0.0.1");
             //This server PORT(Recommend: Using ports that the system is not using)
             int port = 8007;
+            //Define Server Properties
             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try
             {
+                //Configuring server and listen
                 serverSocket.Bind(new IPEndPoint(server_ip, port));
                 serverSocket.Listen(10);
             }
             catch 
             {
+                //If server doesn't open this close or return null.
                 return;
             }
-
+            //Program stop to recieve all messages.
             while (true)
             {
+                //Define client connect
                 Socket clientSocket = serverSocket.Accept();
+                //Await message
                 Thread receive_thread = new Thread(ReceiveMessage);
                 receive_thread.Start(clientSocket);
             }
